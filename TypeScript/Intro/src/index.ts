4e23a692467edd9d443a4e129ccb776a763aafc8
Andres <AndresEM1524@gmail.com>
2023-05-08 15:54:21 -0700

    Covered types and arrays. Also added readme

--- TypeScript/Intro/src/index.ts
+++ TypeScript/Intro/src/index.ts
@@ -1,1 +1,52 @@
-let age: number = 20;
+//-------------------------------------------------
+//Types
+
+// let sales: number = 123_456_789;
+// let course: string = 'TypeScript'; 
+// let is_published: boolean = true;
+
+//Typescript can infer the values of the variables so we don't have to annotate
+
+let sales = 123_456_789;
+let course = 'TypeScript'; 
+let is_published = true;
+
+//assumes type any if we don't specify
+let level;
+//We can re-assign value types with type any 
+level = 1; 
+level = 'a';
+
+//This goes against what typescript is meant for so we avoid type any when we can
+
+//infers document has type any 
+
+// function render(document) {
+//     console.log(document);
+// }
+
+//infers document has type any 
+function render(document: any) {
+    console.log(document);
+}
+
+//-------------------------------------------------
+//Arrays 
+
+//Javascript allows for dynamic types within arrays
+// let numbers = [1,2,'3']; 
+
+//In typescript we can do strict type checking with arrays like so: 
+
+let numbers: number[] = [1,2,3]
+
+// or infer like so 
+
+let numbersInfer = [1,2,3];
+
+//if you want an empty array you have to specify it's type through annotation or it's assumed type any
+
+let numbersEmpty: number [] = [];
+
+//Have properties and methods of, in this case, number objects offered as code completion: 
+    // numbersEmpty.forEach(n => n.)
---
+++ TypeScript/readme.md
@@ -0,0 +1,2 @@
+Based on Programming with Mosh's "TypeScript Tutorial for Beginners"
+https://youtu.be/d56mG7DezGs
