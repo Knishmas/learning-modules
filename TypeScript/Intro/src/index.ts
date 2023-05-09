07b5720b66f681293a7736272e4fdc074864a24c
Andres <AndresEM1524@gmail.com>
2023-05-08 17:04:53 -0700

    Covered enums

--- TypeScript/Intro/src/index.ts
+++ TypeScript/Intro/src/index.ts
@@ -50,3 +50,51 @@ let numbersEmpty: number [] = [];
 
 //Have properties and methods of, in this case, number objects offered as code completion: 
     // numbersEmpty.forEach(n => n.)
+
+//-------------------------------------------------
+/*Tuples
+fixed length array where each element has a particular type, often used with pair of values.
+Note: Tuples should usually be limited to 2 values or else the code can become confusing.
+Example:
+id, name  */
+
+let user: [number, string] = [1, 'Mosh']; 
+/*methods for numbers objects using tuples  
+    user[0].
+and for strings: 
+    user[1].
+methods for array objects: 
+    user.
+    */
+
+//-------------------------------------------------
+/*Enums
+represents a list of related constants
+*/
+
+//Example representing the size of T shirts as constants
+const small = 1; 
+const medium = 2;
+const large = 3; 
+
+// OR can do this through enum 
+/* 
+By default these are numbered: 0,1,2....
+enum Size {Small, Medium, Large}
+
+
+Or you can explicitly set the starting value: 
+    enum Size {Small = 1, Medium, Large}
+Medium becomes 2, large becomes 3 etc.  
+
+
+String values are also allowed, every a value must be explicitly declared for each member 
+    enum Size {Small = 's', Medium = 'm', Large = 'l'}
+    */ 
+
+//If we define our enums then using the const keyword, then the compiler will generate more optimized code that isn't verbose
+
+const enum Size{Small = 1, Medium = 2, Large}; 
+let mySize: Size = Size.Medium;
+console.log(mySize);
+
