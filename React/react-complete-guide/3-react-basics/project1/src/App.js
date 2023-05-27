15cc9505a485a08345a65f22a33f8b2521667915
Andres <AndresEM1524@gmail.com>
2023-05-27 15:13:53 -0700

    Fleshing out expense item component more

--- React/react-complete-guide/3-react-basics/project1/src/App.js
+++ React/react-complete-guide/3-react-basics/project1/src/App.js
@@ -2,7 +2,6 @@ import { ExpenseItem } from "./components/ExpenseItem";
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
       <ExpenseItem/>
     </div>
   );
--- React/react-complete-guide/3-react-basics/project1/src/components/ExpenseItem.js
+++ React/react-complete-guide/3-react-basics/project1/src/components/ExpenseItem.js
@@ -3,7 +3,11 @@ import React from 'react'
 export const ExpenseItem = () => {
   return (
     <div>
-       <h2>Expense item component!</h2>
+       <div>May 27th 2023</div>
+       <div>
+            <h2>Car Insurance</h2>
+            <div>$294.67</div>
+       </div>
     </div>
   )
 }
